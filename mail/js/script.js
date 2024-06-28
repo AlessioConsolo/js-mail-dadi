72b2848f158fe500a98799a867375768e60bb8b1
AlessioConsolo <alessioconsolo3@gmail.com>
2024-06-28 17:53:34 +0200

    [UPD - JS] Added button eventlistener with for cicle inside it

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,3 +7,21 @@ const lista_email = [
 ];
 
 const button = document.getElementById("accedi-button");
+
+button.addEventListener("click", function () {
+  let email = document.getElementById("email").value;
+  let email_confermata = false;
+
+  for (let i = 0; i < lista_email.length; i++) {
+    if (lista_email[i].toLowerCase() === email.toLowerCase()) {
+      email_confermata = true;
+    }
+  }
+
+  if (email_confermata === true) {
+    document.getElementById("messaggio").innerText =
+      "Autenticazione effettuata con successo";
+  } else {
+    document.getElementById("messaggio").innerText = "Autenticazione fallita";
+  }
+});
