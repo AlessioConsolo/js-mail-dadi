58f47d1d87aa1448c928965ceb1c0e2118fd89e1
AlessioConsolo <alessioconsolo3@gmail.com>
2024-06-28 18:15:34 +0200

    [UPD - JS] Added if condition to be able to declare a winner

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,3 +3,11 @@ const Computer = Math.floor(Math.random() * 6) + 1;
 
 console.log("Il punteggio del giocatore è: " + Player);
 console.log("Il punteggio del computer è: " + Computer);
+
+if (Player > Computer) {
+  console.log("Il giocatore ha vinto.");
+} else if (Computer > Player) {
+  console.log("Il computer ha vinto.");
+} else {
+  console.log("Pareggio.");
+}
