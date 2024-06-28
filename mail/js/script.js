5d3bd04827501c0e93a31d32a87a2cdcdf921cfc
AlessioConsolo <alessioconsolo3@gmail.com>
2024-06-28 17:52:39 +0200

    [UPD - JS] Variables lista_email and button added

---
+++ mail/js/script.js
@@ -0,0 +1,9 @@
+const lista_email = [
+  "Crocifissa@gmail.com",
+  "LitterioScalisi@gmail.com",
+  "SalvoLaRosa@gmail.com",
+  "RenatoZero@gmail.com",
+  "AldoBaglio@gmail.com",
+];
+
+const button = document.getElementById("accedi-button");
