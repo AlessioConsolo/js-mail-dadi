320eb6c549bb6aaa4b36a5ece967496d65b411b9
AlessioConsolo <alessioconsolo3@gmail.com>
2024-06-28 18:14:03 +0200

    [UPD - JS] Variable "Player" and "Computer" added and console.log to show the results

---
+++ dadi/js/script.js
@@ -0,0 +1,5 @@
+const Player = Math.floor(Math.random() * 6) + 1;
+const Computer = Math.floor(Math.random() * 6) + 1;
+
+console.log("Il punteggio del giocatore è: " + Player);
+console.log("Il punteggio del computer è: " + Computer);
